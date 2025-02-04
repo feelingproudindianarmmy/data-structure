0cc83581f66a18e0343aa4a4dd1d1a4cbc76183d
feelingproudindianarmy <celestialh4@gmail.com>
2025-02-04 17:50:30 +0545

    lists

---
+++ list.py
@@ -0,0 +1,11 @@
+#creating list
+list1=[2,5,4,"five"]
+list2=[3,6,7,"robin"]
+print(list1)
+print(type(list1))
+#indexing an slicing
+print(list1[1:3])
+print(list1[-1])
+#Adding the list
+list3=list1+list2
+print(list3)
